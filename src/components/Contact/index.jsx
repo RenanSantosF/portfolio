e6490c869f62919ddea3c248891a4cb2357fd84a
Renan Santos <renan_ferreira.es@outlook.com>
2024-03-27 19:46:13 -0300

    fix bugs and add improvements

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -4,6 +4,7 @@ import styles from "./styles.module.css";
 import imgEmailCOntact from "../../assets/email.png";
 import imgGitHub from "../../assets/github.png";
 import imgLinkedIn from "../../assets/linkedin.png";
+import imgWhatsapp from "../../assets/whatsapp.png"
 import ContainerRedes from "./ContainerRedes";
 import ButtonDirect from "./ButtonDirect";
 import ButtonCopy from "./ButtonCopy";
@@ -24,16 +25,22 @@ export default function Contact() {
           TypeButton={<ButtonCopy text="Copiar email" />}
         />
         <ContainerRedes
-          contatoRede="https://www.linkedin.com/in/renan-santos-7946bb213/"
+          contatoRede="linkedin.com/in/renan-santos-7946bb213"
           imgEmailCOntact={imgLinkedIn}
           nomeRede="LinkedIn"
           TypeButton={<ButtonDirect blank='_blank' href='https://www.linkedin.com/in/renan-santos-7946bb213/' text='Ir para a página' />}
         />
         <ContainerRedes
-          contatoRede="https://github.com/RenanSantosF"
+          contatoRede="github.com/RenanSantosF"
           imgEmailCOntact={imgGitHub}
           nomeRede="Git Hub"
           TypeButton={<ButtonDirect blank='_blank' href='https://github.com/RenanSantosF' text='Ir para a página' />}
+        />
+        <ContainerRedes
+          contatoRede="27 998836017"
+          imgEmailCOntact={imgWhatsapp}
+          nomeRede="WhatsApp"
+          TypeButton={<ButtonDirect blank='_blank' href='https://api.whatsapp.com/send/?phone=27998836017&text=Ol%C3%A1,%20Renan!&type=phone_number&app_absent=0' text='Ir para a página' />}
         />
       </div>
     </div>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,46 +8,31 @@ import imgMenuClose from "../../assets/menuClose.png";
 export default function Header({ imgHeader, id }) {
   const [menu, setmenu] = useState(false);
   const [altura, setAltura] = useState(90);
-  const [list, setList] = useState("none");
+  const [largura, setLargura] = useState(1200);
 
   const open = () => {
     setmenu(menu === false ? true : false);
     setAltura(altura === 90 ? 276.44 : 90);
 
-    setTimeout(() => {
-      setList(list == "none" ? "flex"  : "none")
-    }, 300);
-
-    setList(list == "none" ? setTimeout(() => {
-      "flex"
-    },500) : "none")
 
     console.log(list, altura, menu);
   };
 
   return (
     <div id={id} style={{ height: `${altura}px` }} className={styles.container}>
-      <div className={styles.wrapper}>
-        <div className={styles.base}>
-          <div>
-            <img className={styles.imgHeader} src={imgHeader} />
-          </div>
+      <div className={styles.menu}>
+        <img className={styles.imgHeader} src={imgHeader} />
 
-          <a id="openMenu" onClick={open} className={styles.openMenu}>
-            <img src={menu == false ? imgMenuClose : imgMenuOpen} />
-          </a>
-        </div>
-        <div
-          style={{ display: `${list}` }}
-          className={`${
-            menu == false ? styles.listMenuClose : styles.listMenuOpen
-          }`}
-        >
-          <SpanHeader link="#apresentacao" text="Home" />
-          <SpanHeader link="#sobreMim" text="Sobre" />
-          <SpanHeader link="#habilidades" text="Skills" />
-          <SpanHeader link="#contato" text="Contato" />
-        </div>
+        <a id="openMenu" onClick={open} className={styles.openMenu}>
+          <img src={menu == false ? imgMenuClose : imgMenuOpen} />
+        </a>
+      </div>
+
+      <div className={` ${styles.listMenu}   ${menu == true ? styles.active : ''}`}>
+        <SpanHeader link="#apresentacao" text="Home" />
+        <SpanHeader link="#sobreMim" text="Sobre" />
+        <SpanHeader link="#habilidades" text="Skills" />
+        <SpanHeader link="#contato" text="Contato" />
       </div>
     </div>
   );
